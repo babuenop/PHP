1e4488dd78423c8c5ef3c890ae6dc9bd2d5a6a55
Alberto Bueno <abettetto@gmail.com>
2016-09-27 06:33:19 -0500

    Estructura IF PHP
    Practica de estructura IF en Java, calculando funcion de porcentaje

--- Lab4/Lab41.php
+++ Lab4/Lab41.php
@@ -1,16 +1,24 @@
-<html>
-<head>
-<title>Laboratorio 4.1</title>
-</head>
-
-<body>
-
 <?php
+$ventas = 200;
+$valor = $_POST['valor'];
+$porcentaje = $valor/$ventas;
 
-$precio1 = $_POST['precio1'];
-$precio2 = $_POST['precio2'];
-$precio3 = $_POST['precio3'];
-
+if ($porcentaje>=0.80){
+echo "<H2>Resultado</H2>";
+echo "<br/> Presupuesto: $".$ventas."";
+echo "<br/> Porcentaje: ".$porcentaje*'100'."";
+echo "<br/><br/> <img src='http://localhost/DS7/Lab4/Image1.PNG' border='0' width='50' height='50'>";  	
+}
+if ($porcentaje>=0.50 && $porcentaje<0.8){
+echo "<H2>Resultado</H2>";
+echo "<br/> Presupuesto: $".$ventas."";
+echo "<br/> Porcentaje: ".$porcentaje*'100'."";
+echo "<br/><br/> <img src='http://localhost/DS7/Lab4/Image2.PNG' border='0' width='50' height='50'>";  	
+}
+if ($porcentaje<0.50){
+echo "<H1>Resultado</H2>";
+echo "<br/> Presupuesto: $".$ventas."";
+echo "<br/> Porcentaje: ".$porcentaje*'100'."";
+echo "<br/><br/> <img src='http://localhost/DS7/Lab4/Image3.PNG' border='0' width='50' height='50'>";  	
+}
 ?>
-</body>
-</html>
