0a9525262f60a008465dd5a94a385d050569aaa2
Alberto Bueno <abettetto@gmail.com>
2016-11-28 09:34:56 -0500

    Articulo.php
    Define el detalle de cada articulo

---
+++ store/articulo.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+?>
+
+<html>
+  <head>
+		<title>STORE</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+	<?php include "php/navbar.php"; ?>
+
+  <div class="container">
+	 <div class="row">
+     <div class="page-header">
+       <h5>Aqui ira el nombre del articulo<small></small></h5>
+     </div>
+
+     <h4>Pagina en Construccion</h4>
+      <p>Vamos a comprar en Rock&Music <span><a href="./index.php">mas..</a></p>
+      
+
+    </div>
+  </div>
+
+
+</body>
+</html>
