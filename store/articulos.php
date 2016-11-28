4af2ee277889f8749c1f722a1b9f10555277534c
Alberto Bueno <abettetto@gmail.com>
2016-11-27 22:21:25 -0500

    Tienda Virtual en PHP
    Project PHP

---
+++ store/articulos.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+?>
+<html>
+	<head>
+		<title>STORE</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+	<?php include "php/navbar.php"; ?>
+	<div class="container">
+		<div class="row">
+			
+			<table border="2px"> <!-- Lo cambiaremos por CSS -->
+           	<tr>
+              <TD align="center" valign="middle" WIDTH=400><img src="img/1.jpg" class="img-responsive" align="center"></a></TD>
+              <TD WIDTH=400>Celda 2</TD>
+              <TD WIDTH=400>Celda 3</TD>
+          	</tr>
+        	</table>
+        	<br></br>
+        	
+        	<table border="2px"> <!-- Lo cambiaremos por CSS -->
+           	<tr>
+              <TD align="center" valign="middle" WIDTH=400><img src="img/2.jpg" class="img-responsive" align="center"></a></td>
+              <TD WIDTH=400>Celda 2</td>
+              <TD WIDTH=400>Celda 3</td>
+          	</tr>
+        	</table>
+
+        	<br></br>
+
+        	<table border="2px"> <!-- Lo cambiaremos por CSS -->
+           	<tr>
+              <TD align="center" valign="middle" WIDTH=400><img src="img/3.jpg" class="img-responsive" align="center"></a></td>
+              <TD WIDTH=400>Celda 2</td>
+              <TD WIDTH=400>Celda 3</td>
+          	</tr>
+        	</table>
+
+    	</div>
+	</div>
+</body>
+</html>
---
+++ store/php/navbar.php
@@ -0,0 +1,52 @@
+<div class="container">
+  <!-- Topper w/ logo -->
+  <div class="row hidden-xs topper">
+    <div class="col-xs-7 col-sm-7">
+      <a href="#"><img am-TopLogo alt="SECUREVIEW"  src="img/logo.png" class="img-responsive"></a>
+    </div>
+    <div class="col-xs-5 col-xs-offset-1 col-sm-5 col-sm-offset-0 text-right ">
+      <p am-CallNow>Contactenos</p>
+    </div>
+  </div> <!-- End Topper -->
+  <!-- Navigation -->
+  <div class="row">
+    <nav class="navbar navbar-inverse" role="navigation">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand visible-xs-inline-block nav-logo" href="/"><img src="/images/logo-dark-inset.png" class="img-responsive" alt=""></a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+          <ul class="nav navbar-nav js-nav-add-active-class">
+            <?php if(!isset($_SESSION["user_id"])):?>
+            <li><a href="./">Home</a></li>
+            <?php else:?>
+            <li><a href="./">Home</a></li>
+            <li class="active"><a href="./articulos.php">Markets</a></li>
+            <li><a href="/buy.html">Buy</a></li>
+            <li><a href="/svstrike.html">svStrike</a></li>
+            <?php endif;?>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right hidden-xs">
+            <?php if(!isset($_SESSION["user_id"])):?>
+            <a type="button" class="navbar-btn btn btn-gradient-blue" am-latosans="bold" href="./registro.php">Crear Cuenta</a>
+            <a type="button" class="navbar-btn btn btn-gradient-blue" am-latosans="bold" href="./login.php">Login</a>
+            <?php else:?>
+            <a type="button" class="navbar-btn btn btn-gradient-blue" am-latosans="bold" href="./php/logout.php">Salir</a>  
+            <?php endif;?>
+          </ul>
+
+        </div><!-- /.navbar-collapse -->
+      </div>
+    </nav>
+  </div>
+</div>
