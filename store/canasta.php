fd3819bed4e91f87ff618abe131ac83213ad15c4
Alberto Bueno <abettetto@gmail.com>
2016-11-28 09:35:15 -0500

    Pagina de Canasta
    Pagina de Canasta de ariculos

---
+++ store/canasta.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+
+<html>
+  <head>
+		<title>STORE</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+	<?php include "php/navbar.php"; ?>
+
+  <div class="container">
+	 <div class="row">
+     <div class="page-header">
+       <h5>CANASTA<small></small></h5>
+     </div>
+
+     <h4>Tu cesta está vacía.</h4>
+      <p>Haz que tu cesta de compra sea útil: llénala de libros, CD, vídeos, DVD, juguetes, productos electrónicos y otros productos.
+      <p>Vamos a comprar en Rock&Music <span><a href="./index.php">mas..</a></p>
+      
+
+    </div>
+  </div>
+
+
+</body>
+</html>
